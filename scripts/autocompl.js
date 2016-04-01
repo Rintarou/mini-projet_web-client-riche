41f9fa97ecc36f9fb181c631e4e362795897b36c
Pomme-Pomme <lilian.barreteau@laposte.net>
2016-04-01 11:56:44 +0200

    J'aime pas CORS

--- scripts/autocompl.js
+++ scripts/autocompl.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+  var key= "1a2489aa78ceb32dd2478ee997f53e22";
+
   $("#commune").autocomplete({
 
     source : function(request, response){
@@ -39,7 +41,27 @@ $(document).ready(function(){
    $("#boutonDeTest").click(function(event){
       event.preventDefault();
       $("#modale").dialog("open");
-   })
+   });
+
+  $("#recherche").on("click", function(){
+    $.ajax({
+      url:'https://api.flickr.com/services/rest',
+      jsoncallback:'jsonFlickrApi',
+      datatype:'jsonp',
+      type:'GET',
+      data:'method=flickr.photos.search&api_key='+key+'&tags=Nantes&format=json&safe_search=1&per_page=10&callback=jsonpcallback',
+    });
+  });
 
 
 });
+
+
+function jsonFlickrApi(data) {
+
+  for (i in data.photos.photo)
+  {
+    var item = data.photos.photo[i];
+    $("#liste").append("<img src='https://farm"+item.farm+".staticflickr.com/"+item.server+"/"+item.id+"_"+item.secret+".jpg'/><br/><br/>");
+  }
+}
--- scripts/vue_photo.js
+++ scripts/vue_photo.js
@@ -6,26 +6,16 @@ $(document).ready(function(){
     jsoncallback:'jsonFlickrApi',
     datatype:'jsonp',
     type:'GET',
-    data:'method=flickr.photos.search&api_key='+key+'&tags=Paris&format=json&safe_search=1&per_page=10&jsoncallback=?',
-
-    /*success:function(data){
-      console.log(data);
-      $.each(data.photos.photo, function(i, item){
-        $("table").append("<tr><img src=\"https://farm"+item.farm+".staticflickr.com/"+item.server+"/"+item.id+"_"+item.secret+"\"/></tr>");
-      });
-    },*/
+    data:'method=flickr.photos.search&api_key='+key+'&tags=Nantes&format=json&safe_search=1&per_page=10&jsoncallback=?',
   });
 });
 
 function jsonFlickrApi(data) {
-  alert(JSON.stringify(data));
 
   for (i in data.photos.photo)
   {
     var item = data.photos.photo[i];
-    $("table").html("<tr>coucou</tr>");
-    alert(item.server);
-    $("table").html("<tr><img src='https://farm"+item.farm+".staticflickr.com/"+item.server+"/"+item.id+"_"+item.secret+"'/></tr>");
+    $("#liste").append("<img src='https://farm"+item.farm+".staticflickr.com/"+item.server+"/"+item.id+"_"+item.secret+".jpg'/><br/><br/>");
   }
 }
 //https://api.flickr.com/services/rest?method=flickr.photos.search&api_key=1a2489aa78ceb32dd2478ee997f53e22&tags=%22Paris%22
