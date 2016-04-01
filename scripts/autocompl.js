80241e00a50ce701547786b24c6f2de17ce0de79
Pomme-Pomme <lilian.barreteau@laposte.net>
2016-04-01 11:30:43 +0200

    Partie choix de ville

---
+++ scripts/autocompl.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+
+  $("#commune").autocomplete({
+
+    source : function(request, response){
+
+      $.ajax({
+        url:'http://infoweb-ens/~jacquin-c/codePostal/commune.php',
+        type:'GET',
+        dataType:'json',
+        data:'commune='+$("#commune").val()+"&maxRows=10",
+
+        success:function(data){
+          response($.map(data, function(valeur){
+                return {
+                  label:valeur.Ville,
+                  valeur:valeur.Ville,
+                }
+          }));
+        }
+      })
+    },
+
+    minLength:2,
+    select : function(event, ui){
+      $("commune").text(ui.item.value);
+    },
+
+
+  });
+
+   $( "#onglet1" ).tabs();
+
+
+   $("#modale").dialog({
+      autoOpen:false,
+   });
+
+   $("#boutonDeTest").click(function(event){
+      event.preventDefault();
+      $("#modale").dialog("open");
+   })
+
+
+});
